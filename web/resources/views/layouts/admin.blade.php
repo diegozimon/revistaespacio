35681b0e08ea345cd3fc2bbca13b528a549f1c03
diegozimon <52504246+diegozimon@users.noreply.github.com>
2020-07-23 23:05:14 -0400

    vistas
    asdad

--- web/resources/views/layouts/admin.blade.php
+++ web/resources/views/layouts/admin.blade.php
@@ -7,16 +7,16 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="{{asset('stylesheet" href="css/font-awesome.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="css/AdminLTE.min.css">
+    <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="css/_all-skins.min.css">
-    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
-    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="{{asset('css/_all-skins.min.css')}}">
+    <link rel="apple-touch-icon" href="{{asset('img/apple-touch-icon.png')}}">
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
 
   </head>
   <body class="hold-transition skin-blue sidebar-mini">
@@ -191,18 +191,18 @@
       <!--Fin-Contenido-->
       <footer class="main-footer">
         <div class="pull-right hidden-xs">
-          <b>Version</b> 2.3.0
+          <b>Version</b> 1.0.0
         </div>
-        <strong>Copyright &copy; 2015-2020 <a href="www.incanatoit.com">IncanatoIT</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; 22020 <a href="www.incanatoit.com">Rocket Dev Team</a>.</strong> All rights reserved.
       </footer>
 
       
     <!-- jQuery 2.1.4 -->
-    <script src="js/jQuery-2.1.4.min.js"></script>
+    <script src="{{assets('js/jQuery-2.1.4.min.js')}}"></script>
     <!-- Bootstrap 3.3.5 -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="{{assets('js/bootstrap.min.js')}}"></script>
     <!-- AdminLTE App -->
-    <script src="js/app.min.js"></script>
+    <script src="{{assets('js/app.min.js')}}"></script>
     
   </body>
 </html>
--- web/resources/views/revista/usuario/index.blade.php
+++ web/resources/views/revista/usuario/index.blade.php
@@ -1,22 +1,62 @@
 
+@extends('layouts.layout')
+@section('content')
+<div class="row">
+  <section class="content">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-body">
+          <div class="pull-left"><h3>Lista Usuarios</h3></div>
+          <div class="pull-right">
+            <div class="btn-group">
+              <a href="" class="btn btn-info" >Añadir Usuario</a>
+            </div>
+          </div>
+          <div class="table-container">
+            <table id="mytable" class="table table-bordred table-striped">
+             <thead>
+               <th>ID</th>
+               <th>Username</th>
+               <th>Mail</th>
+               <th>Nombre</th>
+               <th>Apellido</th>
+               <th>Ciudad</th>
+               <th>Editar</th>
+               <th>Eliminar</th>
+             </thead>
+             <tbody>
+              @if($usuarios->count())  
+              @foreach($usuarios as $usuario)  
+              <tr>
+                <td>{{$usuario->id}}</td>
+                <td>{{$usuario->username}}</td>
+                <td>{{$usuario->usermail}}</td>
+                <td>{{$usuario->nombre}}</td>
+                <td>{{$usuario->apellido}}</td>
+                <td>{{$usuario->ciudad_id}}</td>
+                <td><a class="btn btn-primary btn-xs" href="{{action('UsuarioController@edit', $usuario->id)}}" ><span class="glyphicon glyphicon-pencil"></span></a></td>
+                <td>
+                  <form action="{{action('UsuarioController@destroy', $usuario->id)}}" method="post">
+                   {{csrf_field()}}
+                   <input name="_method" type="hidden" value="DELETE">
 
-<h3>Index</h3>
-<table id="example1" width="100%">
-    <thead class="thead-default">
-    <tr>
-        <th>ID</th>
-        <th>Usuario</th>
-        <th>Correo</th>
-    </tr>
-    </thead>
-    <tbody>
-	@foreach($usuarios as $usuario)
-    <tr>
-        <td>{{ $usuario->id }}</td>
-        <td>{{ $usuario->username }}</td>
-        <td>{{ $usuario->usermail }}</td>
-    </tr>
-	@endforeach
-    </tbody>
-</table>
+                   <button class="btn btn-danger btn-xs" type="submit"><span class="glyphicon glyphicon-trash"></span></button>
+                 </td>
+               </tr>
+               @endforeach 
+               @else
+               <tr>
+                <td colspan="8">No hay registro !!</td>
+              </tr>
+              @endif
+            </tbody>
 
+          </table>
+        </div>
+      </div>
+      {{ $usuarios->links() }}
+    </div>
+  </div>
+</section>
+
+@endsection
