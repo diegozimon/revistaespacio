909ba551bf84ff8e219f5972b4da4c7a422f69fc
diegozimon <52504246+diegozimon@users.noreply.github.com>
2020-07-23 23:46:02 -0400

    create-edit
    asdsad

---
+++ web/resources/views/revista/usuario/edit.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.layout')
+@section('content')
+<div class="row">
+	<section class="content">
+		<div class="col-md-8 col-md-offset-2">
+			@if (count($errors) > 0)
+			<div class="alert alert-danger">
+				<strong>Error!</strong> Revise los campos obligatorios.<br><br>
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+			@endif
+			@if(Session::has('success'))
+			<div class="alert alert-info">
+				{{Session::get('success')}}
+			</div>
+			@endif
+
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Editar Usuario</h3>
+				</div>
+				<div class="panel-body">					
+					<div class="table-container">
+						<form method="POST" action="{{ route('libro.update',$libro->id) }}"  role="form">
+							{{ csrf_field() }}
+							<input name="_method" type="hidden" value="PATCH">
+							<div class="row">
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="form-group">
+										<input type="text" name="username" id="username" class="form-control input-sm" value="{{$usuario->username}}">
+									</div>
+								</div>
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="form-group">
+										<input type="text" name="usermail" id="usermail" class="form-control input-sm" value="{{$usuario->usermail}}">
+									</div>
+								</div>
+							</div>
+
+				<!--			<div class="form-group">
+								<textarea name="resumen" class="form-control input-sm"  placeholder="Resumen">{{$usuario->resumen}}</textarea>
+							</div> -->
+							<div class="row">
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="form-group">
+										<input type="text" name="nombre" id="nombre" class="form-control input-sm" value="{{$usuario->nombre}}">
+									</div>
+								</div>
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="form-group">
+										<input type="text" name="apellido" id="apellido" class="form-control input-sm" value="{{$usuario->apellido}}">
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<textarea name="autor" class="form-control input-sm" placeholder="Autor">{{$usuario->apellido}}</textarea>
+							</div>
+							<div class="row">
+
+								<div class="col-xs-12 col-sm-12 col-md-12">
+									<input type="submit"  value="Actualizar" class="btn btn-success btn-block">
+									<a href="{{ route('revista.usuario.index') }}" class="btn btn-info btn-block" >Atrás</a>
+								</div>	
+
+							</div>
+						</form>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	@endsection
